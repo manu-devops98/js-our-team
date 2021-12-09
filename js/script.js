d7758c42829e4d59f239694f518ff78acb3ad866
manu-devops98 <manuelcappello7@gmail.com>
2021-12-09 13:35:47 +0100

    add esercizio

--- js/script.js
+++ js/script.js
@@ -30,3 +30,34 @@ const team = [
     image: 'barbara-ramos-graphic-designer.jpg',
   },
 ];
+
+// creo funzione per stampare le cards del team 
+function printTeam(array, container) {
+  container.innerHTML = '';
+  // creo ciclo for per ciclare sull'array di oggetti 
+  for (let i = 0; i < array.length; i++) {
+    const obj = array[i];
+// creo variabile dove inserire in modo dinamico immagini,ruolo e nome 
+    const teamCard = `
+      <div class="team-card">
+        <div class="card-image">
+            <img
+              src="img/${obj.image}"
+              alt="${obj.name}"
+            />
+        </div>
+        <div class="card-text">
+            <h3>${obj.name}</h3>
+            <p>${obj.role}</p>
+        </div>
+      </div>
+    `;
+    // inserisco variabile in html 
+    container.innerHTML += teamCard; 
+  }
+}
+// creo variabile contenitore
+const teamContainer = document.querySelector('.team-container');
+// utilizzo funzione per stampare array in html 
+printTeam(team, teamContainer);
+
